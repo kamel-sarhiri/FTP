461377dcb54021b8cccd42c911c4b24d2234b5d0
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-10-06 17:25:31 +0200

    Job 7 MAJ script

--- job7/installftp.sh
+++ job7/installftp.sh
@@ -2,7 +2,7 @@
 
 echo "\e[5m$(tput setaf 7)$(tput setab 1)veuillez passer en root pour que le script fonctionne correctement \e[0m"
 
-echo "Ce message s'autodétruira dans 10 secondes. :D"
+echo "\e[5m$(tput setaf 7)$(tput setab 1)Ce message s'autodétruira dans 10 secondes. :D \e[0m"
 sleep 10
 ##############################################
 #install proftpd et client filezilla
