b0549ff06e59f2c93ca0e4cab5d1adeaefa552f0
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-10-06 10:49:59 +0200

    Job 8 ajout suppression openssl

--- job8/uninstallftp.sh
+++ job8/uninstallftp.sh
@@ -2,3 +2,4 @@
 
 sudo apt remove proftpd* && sudo apt autoremove proftpd* && sudo apt purge proftpd*
 sudo apt remove filezilla && sudo apt autoremove filezila 
+sudo apt remove openssl && sudo apt autoremove openssl 
