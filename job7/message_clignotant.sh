c7f06b3b8beb27dfd53aadc28d236977fec01767
kamel-sarhiri <kamel.sarhiri@laplateforme.io>
2022-10-07 09:59:36 +0200

    MAJ script message clignotant

--- job7/message_clignotant.sh
+++ job7/message_clignotant.sh
@@ -1,1 +1,3 @@
 echo  "\e[5m$(tput setaf 7)$(tput setab 1)veuillez passer en root pour que le script fonctionne correctement \e[0m"
+echo "\e[5m$(tput setaf 7)$(tput setab 1)Ce message s'autodétruira dans 10 secondes. :D \e[0m"
+sleep 10
